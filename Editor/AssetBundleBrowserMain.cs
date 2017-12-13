82f48459f5d423d698609362bc1c67f5d323fa1a
Bill Ramsour <bill@unity3d.com>
2017-12-13 14:36:05 -0600

    getting test assemblies to work.

--- Editor/AssetBundleBrowserMain.cs
+++ Editor/AssetBundleBrowserMain.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleToAttribute("com.unity.assetbundlebrowser.EditorTests")]
+
 namespace AssetBundleBrowser
 {
 
