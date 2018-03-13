e4ccca2dc71b56b1bd5e6bc5642ede2924e875b3
artofcode <nerbert@gmail.com>
2018-03-13 17:34:16 +0000

    Update ABModelAssetInfo.cs

--- Editor/AssetBundleModel/ABModelAssetInfo.cs
+++ Editor/AssetBundleModel/ABModelAssetInfo.cs
@@ -176,7 +176,7 @@ namespace AssetBundleBrowser.AssetBundleModel
             if (m_dependencies != null && m_dependencies.Count > 0)
             {
                 var message = string.Empty;
-				List<AssetInfo> sortedDependencies = m_dependencies.OrderBy(d => d.bundleName).ToList();
+                List<AssetInfo> sortedDependencies = m_dependencies.OrderBy(d => d.bundleName).ToList();
                 foreach (var dependent in sortedDependencies)
                 {
                     if (dependent.bundleName != bundleName)
